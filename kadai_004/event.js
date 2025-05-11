70300908cd188fe3aad7bd10a8d95916001c661f
masatoshi-51 <t.o.u.s08411@ezweb.ne.jp>
2025-05-12 00:32:19 +0900

    課題提出

---
+++ kadai_004/event.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(window).on('load',()=> {
+        console.log('loadイベントが発生しました');
+    });
+    
+    $(window).on('scroll',()=> {
+        console.log('scrollイベントが発生しました');
+    });
+});
+
